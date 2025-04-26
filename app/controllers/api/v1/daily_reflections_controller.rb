b91d65f544d593dac218d454e43b472536afb67e
daichi takei <dadewujing0@gmail.com>
2025-04-26 20:46:29 +0900

    add: ratingと名前の修正

--- app/controllers/api/v1/daily_reflections_controller.rb
+++ app/controllers/api/v1/daily_reflections_controller.rb
@@ -2,54 +2,52 @@
 module Api
     module V1
         class DailyReflectionsController < ApplicationController
-        before_action :set_DailyReflection, only: %i[ show update destroy ]
+        before_action :set_daily_reflection, only: %i[ show update destroy ]
 
         # GET /DailyReflections
         def index
-            @DailyReflections = DailyReflection.all
+            @daily_reflections = DailyReflection.all
 
-            render json: @DailyReflections
+            render json: @daily_reflections
         end
 
         # GET /DailyReflections/1
         def show
-            render json: @DailyReflection
+            render json: @daily_reflection
         end
 
         # POST /DailyReflections
         def create
-            @DailyReflection = DailyReflection.new(DailyReflection_params)
+            @daily_reflection = DailyReflection.new(daily_reflection_params)
 
-            if @DailyReflection.save
-            render json: @DailyReflection, status: :created, location: @DailyReflection
-            else
-            render json: @DailyReflection.errors, status: :unprocessable_entity
+            if @daily_reflection.save
+            render json: @daily_reflection, status: :created
             end
         end
 
         # PATCH/PUT /DailyReflections/1
         def update
-            if @DailyReflection.update(DailyReflection_params)
-            render json: @DailyReflection
+            if @daily_reflection.update(daily_reflection_params)
+            render json: @daily_reflection
             else
-            render json: @DailyReflection.errors, status: :unprocessable_entity
+            render json: @daily_reflection.errors, status: :unprocessable_entity
             end
         end
 
         # DELETE /DailyReflections/1
         def destroy
-            @DailyReflection.destroy!
+            @daily_reflection.destroy!
         end
 
         private
             # Use callbacks to share common setup or constraints between actions.
             def set_DailyReflection
-            @DailyReflection = DailyReflection.find(params.expect(:id))
+                @daily_reflection = DailyReflection.find(params.expect(:id))
             end
 
             # Only allow a list of trusted parameters through.
-            def DailyReflection_params
-            params.require(:DailyReflection).permit(:name)
+            def daily_reflection_params
+                params.require(:daily_reflection).permit(:comment,rating)
             end
         end
     end
--- db/migrate/20250426014814_create_daily_reflections.rb
+++ db/migrate/20250426014814_create_daily_reflections.rb
@@ -2,7 +2,7 @@ class CreateDailyReflections < ActiveRecord::Migration[8.0]
   def change
     create_table :daily_reflections do |t|
       t.string :comment
-
+      t.integer :rating
       t.timestamps
     end
   end
