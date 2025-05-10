a17ee220e30fcdd81f0ad17bfb4d27b8f9d0426a
daichi takei <dadewujing0@gmail.com>
2025-05-10 20:06:54 +0900

    add: cors.rbに複数のoriginを追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "https://today-star.vercel.app"
+        origins "https://today-star.vercel.app","https://today-star.cat-and-mountain.com"
         resource "*",
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
